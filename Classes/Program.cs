182a5db4e3264e74ef2cbfa93236f9591243e78c
TonyDolenti <antoniobdolenti@gmail.com>
2023-02-14 21:01:25 -0500

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Classes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Nissan";
+            myCar.Model = "350z";
+            myCar.Year = 2006;
+
+            var otherCar = new Car()
+            {
+                Make = "Subaru",
+                Model = "BRZ",
+                Year = 2013,
+            };
+
+            var nissan = new Car($"Nissan", "GTR", 2009);
+
+            var carList = new List<Car>() { myCar, otherCar, nissan };
+            
+            foreach(var car in carList)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
+
+
         }
     }
 }
